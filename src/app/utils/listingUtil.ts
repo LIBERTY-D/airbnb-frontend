015ca3d7c3e552d37198910695e487ad9d29e7ff
LIBERTY-D <danielliberty2425@gmail.com>
2025-07-12 16:34:53 +0200

    updated listingUtil.ts

--- src/app/utils/listingUtil.ts
+++ src/app/utils/listingUtil.ts
@@ -1,13 +1,19 @@
 import { AirbnbListing } from '../types';
+
 export function modifyListing(data: AirbnbListing) {
-  if (data.images?.main) {
-    data.images.main = `data:image/png;base64,${data.images.main}`;
+  if (data.images?.main && typeof data.images.main === 'string') {
+    if (!data.images.main.startsWith('data:image/')) {
+      data.images.main = `data:image/png;base64,${data.images.main}`;
+    }
   }
 
   if (Array.isArray(data.images?.gallery)) {
-    data.images.gallery = data.images.gallery.map(
-      (g) => `data:image/png;base64,${g}`
-    );
+    data.images.gallery = data.images.gallery.map((g) => {
+      if (typeof g === 'string') {
+        return g.startsWith('data:image/') ? g : `data:image/png;base64,${g}`;
+      }
+      return g;
+    }) as string[];
   }
 
   return data;
